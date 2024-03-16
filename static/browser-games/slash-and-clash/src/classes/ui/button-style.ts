527ce9e5100f5fc7780286baac568215e3611ea7
Gad Wissberg <gadw17@gmail.com>
2024-03-16 22:58:20 +0200

    Adding game.

--- static/browser-games/slash-and-clash/src/classes/ui/button-style.ts
+++
@@ -1,5 +0,0 @@
-import { Images } from "../../scenes/loading/assets-definitions";
-
-export class ButtonStyle {
-    constructor(readonly textureUp: Images, readonly textureDown: Images, readonly textureOver: Images) { }
-}
